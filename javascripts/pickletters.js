6e0d081bc4c2a76545ad14e56d2ad7fe7c85212f
Mike Eng <mrengy@gmail.com>
2012-08-30 23:58:43 -0400

    cleaned up pickletters.js

--- javascripts/pickletters.js
+++ javascripts/pickletters.js
@@ -1,29 +1,17 @@
 //cycles through each character and tries to make a word with it
+
+//initializes array
 var options = [];
+
+//builds nested array with each option defined for each letter
 var buildOptions = function(){
 	$('.options').each(function(){
 		var theseoptions = $.trim($(this).html());
 		var theseoptionsArr = theseoptions.split('');
 		options.push(theseoptionsArr);
-		//console.log(theseoptionsArr);
-		/*
-		options.push([
-			for (var i=0; i<theseoptions.length; i++){
-			   return '\".theseoptionsArr[i].\"., ';
-			}
-		]);
-		*/
 	});
 };
 
 $(document).ready(function() {
-    /*
-	var options = [
-		["G", "H", "I"],
-		["D", "E", "F"]
-	];
-	options.push(["J", "K", "L"]);
-	console.log(options[2][1]);
-	*/
 	buildOptions();
 });
