4a266a5e6d54a1d87cfe66a04c297b8409812025
Mike Eng <mrengy@gmail.com>
2012-08-30 21:13:31 -0400

    output contents of message with php and generated basic markup

---
+++ decoder.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>Digit Decoder</title>
+  
+  <!-- Included CSS Files (Uncompressed) -->
+  <!--
+  <link rel="stylesheet" href="stylesheets/foundation.css">
+  -->
+  
+  <!-- Included CSS Files (Compressed) -->
+  <link rel="stylesheet" href="stylesheets/foundation.min.css">
+  <link rel="stylesheet" href="stylesheets/app.css">
+
+  <link rel="stylesheet" href="stylesheets/decoder.css">
+
+  <script src="javascripts/modernizr.foundation.js"></script>
+
+  <!-- IE Fix for HTML5 Tags -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+<body>
+
+  <div class="row">
+	<div class="twelve columns">
+      <h1>Digit Decoder</h1>
+		<pre>
+			<?php 
+				$filename = 'message.txt';
+				$file = fopen($filename, 'r');
+				$contents = fread($file, filesize($filename));
+				fclose($file);
+				
+				//create array with numbers in the message
+				$numbers = str_getcsv($contents);
+			?>
+		</pre>
+		<br/>
+      	<?php
+      		foreach ($numbers as $number){
+		?>
+				<div class="character">
+					<div class="number">
+						<?php echo $number;?>
+					</div>
+					<div class="options">
+						abc
+					</div>
+					<div class="letter">
+						a
+					</div>
+				</div>
+		<?php
+			}
+      	?>
+	</div>
+  </div>
+<!--
+	<div class="twelve columns">
+		<div class="character one column">
+			<div class="number">
+				2
+			</div>
+			<div class="options">
+				ABC
+			</div>
+			<div class="letter">
+				A
+			</div>
+		</div>
+		<div class="character one column">
+			<div class="number">
+				3
+			</div>
+			<div class="options">
+				DEF
+			</div>
+			<div class="letter">
+				D
+			</div>
+		</div>
+	</div>
+-->
+  
+  
+  
+  <!-- Included JS Files (Uncompressed) -->
+  <!--
+  
+  <script src="javascripts/modernizr.foundation.js"></script>
+  
+  <script src="javascripts/jquery.js"></script>
+  
+  <script src="javascripts/jquery.foundation.mediaQueryToggle.js"></script>
+  
+  <script src="javascripts/jquery.foundation.navigation.js"></script>
+  
+  <script src="javascripts/jquery.foundation.buttons.js"></script>
+  
+  <script src="javascripts/jquery.foundation.tabs.js"></script>
+  
+  <script src="javascripts/jquery.foundation.forms.js"></script>
+  
+  <script src="javascripts/jquery.foundation.tooltips.js"></script>
+  
+  <script src="javascripts/jquery.foundation.accordion.js"></script>
+  
+  <script src="javascripts/jquery.placeholder.js"></script>
+  
+  <script src="javascripts/jquery.foundation.alerts.js"></script>
+  
+  -->
+  
+  <!-- Included JS Files (Compressed) -->
+  <script src="javascripts/foundation.min.js"></script>
+  
+  <!-- Initialize JS Plugins -->
+  <script src="javascripts/app.js"></script>
+</body>
+</html>
