5c25dcca1b0bd11fa22cbfe1144fcc57581d9c18
Mike Eng <mrengy@gmail.com>
2012-08-30 21:22:14 -0400

    removed old processing file

---
+++ digits-to-letters.php
@@ -0,0 +1,11 @@
+<?php
+//defines array for converting digits to possible letters
+
+$d2l[2] = "ABC";
+$d2l[3] = "DEF";
+$d2l[4] = "GHI";
+$d2l[5] = "JKL";
+$d2l[6] = "MNO";
+$d2l[7] = "PQRS";
+$d2l[8] = "TUV";
+$d2l[9] = "WXYZ";
