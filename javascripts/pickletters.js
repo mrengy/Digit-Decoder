ad1cbbebeac4aff3277f01d587c63ce29a21adae
Mike Eng <mrengy@gmail.com>
2012-09-04 08:28:39 -0400

    building checkGuessWord function

--- javascripts/pickletters.js
+++ javascripts/pickletters.js
@@ -3,6 +3,8 @@
 //initializes variables
 var options = [];
 var guessWord;
+var maxWordLength = 10;
+var foundWordIndex = -1;
 
 //builds nested array with each option defined for each letter
 var buildOptions = function(){
@@ -13,13 +15,25 @@ var buildOptions = function(){
 	});
 };
 
-//steps through each character to check for defined word
+//compares guess word to accepted word list
+var checkGuessWord = function(){
+	// shows index of words array in which a guessword appears, if any. If no match, foundWordIndex = -1.
+	foundWordIndex = $.inArray(guessWord, words);
+	if (foundWordIndex > -1){
+		break;
+		console.log(guessWord);
+		return foundWordIndex;
+	}
+}
+
+//steps through each character to build a guess word
 var buildGuessWord = function(){
-	options.each(function(){
-		//guessWord.= 
-	});
+	
+	//guessWord.= 
+	
 
 };
 $(document).ready(function() {
 	buildOptions();
+	//console.log(options);
 });
