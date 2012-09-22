55ff3a08f549dd76fce937d8d4fb2c83b0daa73b
Mike Eng <mrengy@gmail.com>
2012-09-22 17:43:44 -0400

    trying another approach with timeout to find dict.AAH - still returning undefined

--- javascripts/words.js
+++ javascripts/words.js
@@ -4,19 +4,52 @@
 //http://ejohn.org/blog/dictionary-lookups-in-javascript/
 var dict = {};
 
+//approach using timeout as well as callback, still showing dict.AA as undefined.
+
+$.get("dictionary.txt", function(txt){
+	var words = txt.split("\n");
+	
+	for (var i=0; i<words.length; i++){
+		dict[words[i]] = true;
+	}
+	
+	findTimer = setTimeout(function () {
+		findWord('AA');
+	}, 1000);
+});
+
+function findWord(letters){
+	word = letters;
+	console.log(dict);
+	if (dict[word]){
+		console.log(word+' is a word');
+	}
+	else {
+		console.log(word+' has no dice');
+	}
+	console.log(dict);
+}
+
+
 /*
 //ajax call to read dictionary.txt file
-$.get("dictionary.txt", function( txt ){
+$.get("dictionary.txt", parseResults);
+
+
+function parseResults(txt) {
     var words = txt.split( "\n");
 
     for (var i=0; i < words.length; i++){
         dict[ words[i] ] = true;
     }
 
-    //Now inside these console.log will run once you DO have the data
     console.log(dict.AAH);
     console.log(dict);
-});
+
+    if (dict.AAH == true) {
+       console.log('dict.AAH is true!');
+    }
+}
 */
 /*
 function getDictionary(){
