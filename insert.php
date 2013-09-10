199d067b54fe34ac7a3e4849eb2f8df24c194494
Mike Eng <mike@buildium.com>
2013-09-10 18:39:37 -0400

    AJAX call working with dummy data

--- insert.php
+++ insert.php
@@ -1,24 +1,21 @@
 <?php
-//configure and connect to the database
-$con = mysql_connect('localhost','digit_decoder','schell', 'digit_decoder');
-if(!$con){
-	die('Error: Could not connect to database. '. mysql_error());
-}
-
-mysql_select_db('users', $con);
 //pull data from ajax call
 $id = $_POST['ID'];
 $username = $_POST['username'];
 $message = $_POST['message'];
 
-$query = mysql_query("INSERT INTO users(username,message) VALUES('$username','$message')");
-if($query){
-	echo("data for $username inserted successfully");
-}
-else{
-	echo("Error posting data");
+//configure and connect to the database
+@ $con = new mysqli('localhost','digit_decoder','schell', 'digit_decoder');
+
+if(mysqli_connect_errno()){
+	echo('Error: Could not connect to database.');
+	exit;
 }
 
+$query = "INSERT INTO users(username,message) VALUES('$username','$message')";
+$con->query($query);
+
+$con->close();
 
 
 ?>
