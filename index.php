eb629eb24786eb4b1800fc7189228df1813990dd
Mike Eng <mike@buildium.com>
2013-10-04 09:10:27 -0400

    refactoring login to only use index.php

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
-<?php include ('header.php'); ?>
-<?php include ('initial-message.php'); ?>
-<?php include ('footer.php'); ?>
+<?php
+include('header.php');
+
+if (isset($_SESSION['email'])){
+	//if the user is logged in, query the database for the stored message
+	if (strpos($_SERVER['HTTP_HOST'],'localhost') !== false){
+	  	//local version
+		include_once('db-connect.php');
+	} else{
+	  	//remote version
+	  	include_once('db-connect-sandbox.php');	
+	}
+
+	$username = $_SESSION['email'];
+
+	$query = "SELECT message FROM `users` WHERE ((`users`.`username` = '$username'))";
+	$result = $db->query($query);
+
+	//print out the result
+	if ($result){
+		$row = $result->fetch_assoc();
+		if(strlen($row['message']) > 0){
+			//only display the stored message if there is one
+			echo base64_decode($row['message']);
+		} else {
+			//otherwise display the initial message
+			include ('initial-message.php');
+		}
+	} else{
+		echo "Error reading from database.";
+	}
+	$db->close();
+} else{
+	//if the user is not logged in, display the initial message
+	include ('initial-message.php');
+}
+include('footer.php');
+?>
