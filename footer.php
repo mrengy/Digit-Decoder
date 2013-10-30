8078a92df9c42e8d5fbe3b05285efd2928bf1c8d
Mike Eng <mrengy@gmail.com>
2013-10-30 18:12:25 -0400

    adding google analytics tracking code

--- footer.php
+++ footer.php
@@ -56,5 +56,14 @@
   </script>
   <script src="javascripts/words.js"></script>
   <script src="javascripts/pickletters.js"></script>
+  <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-45247844-1', 'mike-eng.com');
+	  ga('send', 'pageview');
+  </script>
 </body>
 </html>
