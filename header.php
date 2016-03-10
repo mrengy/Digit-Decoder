1106221cefaae80b604b8c1c479b0f98831b5fde
Mike Eng <mrengy@gmail.com>
2016-03-10 09:54:11 -0500

    removed login functionality

--- header.php
+++ header.php
@@ -1,26 +1,3 @@
-<?php include ('example-google.php'); ?>
-<?php
-
-/*
-// hardcoded version for offline debugging
-session_start();
-$_SESSION['email'] = 'nobody3';
-*/
-
-//handling logout
-if(isset($_GET['logout'])){
-	//log out of gmail
-	echo('<script type="text/javascript" 
-	    src="https://mail.google.com/mail/u/0/?logout&hl=en"> </script>');
-	
-	//log out of this app
-	unset($_SESSION['email']);
-	unset($_SESSION['insert-id']);
-	//echo "session var insert-id unset";
-	session_destroy();
-}
-
-?>
 <!DOCTYPE html>
 
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
@@ -60,35 +37,9 @@ if(isset($_GET['logout'])){
 <body>
 
   <div class="row" id="title-row">
-	<div class="eight columns">
+	<div class="twelve columns">
       <h1 id="title">Digit Decoder</h1>
 	  <a href="https://github.com/mrengy/digit-decoder#digit-decoder" id="what-is-this">What is this?</a>
-	</div>
-	<div id="global-controls" class="four columns">
-		<span id="global-controls-contents">
-	 	 <!--<a href="#" data-reveal-id="loginModal">Log in</a>-->
-		  <?php
-		  	if (isset($_SESSION['email'])){
-		  ?>
-			  <div id="logged-in-as">
-				<?php echo($_SESSION['email']); ?>
-				<form action="index.php?logout" method="post"> 
-					<button id="logout" class="link-style" name="logout">Log out</button>
-				</form>
-			  </div>
-			  <a id="save" class="button">Save</a>
-		 <?php	 
-		 	} else{
-			//if session variable email is not set
-	     ?>
-		  	<form action="?login" method="post">
-			    <button class="button" name="login">Login with Google to save</button>
-			</form>
-			<a href="#" class="vspace" id="buttonSecurity" data-reveal-id="modalSecurity">Is this secure?</a>
-		<?php		
-	  		}
-	  	?>
-		</span>
 	</div>
   </div>
   <div class="row" id="message">
